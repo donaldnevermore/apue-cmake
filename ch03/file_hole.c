f17e8f02a7032b5bfc810c12a1bad4146ae582f8
dcwong <donald33wang@gmail.com>
2025-04-27 13:13:45 +0000

    Move files to ch03

--- ch03/file_hole.c
+++ ch03/file_hole.c
@@ -1,10 +1,10 @@
 #include "apue.h"
 #include <fcntl.h>
 
-// pic 3-2
 char buf1[] = "abcdefghij";
 char buf2[] = "ABCDEFGHIJ";
 
+// fig 3-2
 int main(void) {
     int fd;
     if ((fd = creat("file.hole", FILE_MODE)) < 0) {
@@ -19,7 +19,7 @@ int main(void) {
         err_sys("lseek error");
     }
 
-    if (write(fd, buf2, 10) == 10) {
+    if (write(fd, buf2, 10) != 10) {
         err_sys("buf2 write error");
     }
 
